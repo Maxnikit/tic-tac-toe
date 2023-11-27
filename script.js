4c4f6e4ec35f6d64a6436516f71b24a073f6fb98
Maxnikit <max.nikit.03@gmail.com>
2023-11-27 18:52:35 +0300

    fix CheckForWin

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const gameBoard = (function () {
     if (board[row][col] === 0) {
       board[row][col] = mark;
     }
+    console.table(board);
   };
   const printBoard = () => {
     const boardWithMarks = board.map((row) =>
@@ -20,36 +21,44 @@ const gameBoard = (function () {
     );
     return console.table(boardWithMarks);
   };
-  const checkForWin = function (board) {
-    function threeInRow(board) {
+  const checkForWin = function (mark) {
+    function threeInRow() {
+      let j = 0;
       for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          if (
-            board[i][j] === board[i][j + 1] &&
-            board[i][j] === board[i][j + 2]
-          ) {
-            return true;
-          }
+        if (
+          board[i][j] === board[i][j + 1] &&
+          board[i][j] === board[i][j + 2] &&
+          board[i][j] === mark
+        ) {
+          return true;
         }
       }
     }
-    function threeInCol(board) {
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          if (
-            board[i][j] === board[i + 1][j] &&
-            board[i][j] === board[i + 2][j]
-          ) {
-            return true;
-          }
+    function threeInCol() {
+      let i = 0;
+      for (let j = 0; j < cols; j++) {
+        if (
+          board[i][j] === board[i + 1][j] &&
+          board[i][j] === board[i + 2][j] &&
+          board[i][j] === mark
+        ) {
+          return true;
         }
       }
     }
-    function threeInDiag(board) {
-      if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    function threeInDiag() {
+      if (
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2] &&
+        board[1][1] === mark
+      ) {
         return true;
       }
-      if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+      if (
+        board[0][2] === board[1][1] &&
+        board[0][2] === board[2][0] &&
+        board[1][1] === mark
+      ) {
         return true;
       }
     }
@@ -79,46 +88,11 @@ const createPlayer = function (name) {
     giveScore,
   };
 };
-const Max = createPlayer("Max");
 const gameLogic = function () {};
 
-const checkForWin = function (board) {
-  function threeInRow(board) {
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        if (
-          board[i][j] === board[i][j + 1] &&
-          board[i][j] === board[i][j + 2]
-        ) {
-          return true;
-        }
-      }
-    }
-  }
-  function threeInCol(board) {
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        if (
-          board[i][j] === board[i + 1][j] &&
-          board[i][j] === board[i + 2][j]
-        ) {
-          return true;
-        }
-      }
-    }
-  }
-  function threeInDiag(board) {
-    if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-      return true;
-    }
-    if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-      return true;
-    }
-  }
-
-  if (threeInRow(board) || threeInCol(board) || threeInDiag(board)) {
-    return true;
-  }
-  return false;
-};
-// TODO Check for win is not working. Cannon read properties of "0"
+const Max = createPlayer("Max");
+gameBoard.putMark(1, 0, -1);
+gameBoard.putMark(1, 1, -1);
+gameBoard.putMark(1, 2, -1);
+console.log(`-1(O) win check:  ${gameBoard.checkForWin(-1)}`);
+console.log(`+1(X) win check:  ${gameBoard.checkForWin(1)}`);
