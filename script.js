5397752512dfc3175714cbd84babd98e76e1ccb7
Maxnikit <max.nikit.03@gmail.com>
2023-11-27 19:13:05 +0300

    Add StartNewRound

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const gameBoard = (function () {
   const rows = 3;
   const cols = 3;
   const board = [];
-  for (i = 0; i < rows; i++) {
-    board[i] = [];
-    for (j = 0; j < cols; j++) {
-      board[i][j] = 0;
+  const createBoard = () => {
+    for (i = 0; i < rows; i++) {
+      board[i] = [];
+      for (j = 0; j < cols; j++) {
+        board[i][j] = 0;
+      }
     }
-  }
+    return board;
+  };
+
   const getBoard = () => board;
   const putMark = (row, col, mark) => {
     if (board[row][col] === 0) {
@@ -21,10 +25,28 @@ const gameBoard = (function () {
     );
     return console.table(boardWithMarks);
   };
+
+  return { createBoard, getBoard, putMark, printBoard };
+})();
+const createPlayer = function (name) {
+  let score = 0;
+  const getScore = () => score;
+  const giveScore = () => {
+    score++;
+  };
+  return {
+    name,
+    getScore,
+    giveScore,
+  };
+};
+const gameLogic = (function () {
+  let board = gameBoard.getBoard();
+
   const checkForWin = function (mark) {
     function threeInRow() {
       let j = 0;
-      for (let i = 0; i < rows; i++) {
+      for (let i = 0; i < 3; i++) {
         if (
           board[i][j] === board[i][j + 1] &&
           board[i][j] === board[i][j + 2] &&
@@ -36,7 +58,7 @@ const gameBoard = (function () {
     }
     function threeInCol() {
       let i = 0;
-      for (let j = 0; j < cols; j++) {
+      for (let j = 0; j < 3; j++) {
         if (
           board[i][j] === board[i + 1][j] &&
           board[i][j] === board[i + 2][j] &&
@@ -68,31 +90,26 @@ const gameBoard = (function () {
     }
     return false;
   };
-
-  return {
-    getBoard,
-    putMark,
-    printBoard,
-    checkForWin,
+  const startNewRound = () => {
+    board = gameBoard.getBoard();
+    // gameBoard.printBoard();
+    if (gameLogic.checkForWin(-1)) {
+      console.log("0 won round");
+    }
+    if (gameLogic.checkForWin(1)) {
+      console.log("X won round");
+    }
+    gameBoard.createBoard();
   };
+  return { checkForWin, startNewRound };
 })();
-const createPlayer = function (name) {
-  let score = 0;
-  const getScore = () => score;
-  const giveScore = () => {
-    score++;
-  };
-  return {
-    name,
-    getScore,
-    giveScore,
-  };
-};
-const gameLogic = function () {};
 
 const Max = createPlayer("Max");
+gameBoard.createBoard();
 gameBoard.putMark(1, 0, -1);
 gameBoard.putMark(1, 1, -1);
 gameBoard.putMark(1, 2, -1);
-console.log(`-1(O) win check:  ${gameBoard.checkForWin(-1)}`);
-console.log(`+1(X) win check:  ${gameBoard.checkForWin(1)}`);
+console.log(`-1(O) win check:  ${gameLogic.checkForWin(-1)}`);
+console.log(`+1(X) win check:  ${gameLogic.checkForWin(1)}`);
+console.log(gameLogic.startNewRound());
+gameBoard.putMark(1, 1, 1);
