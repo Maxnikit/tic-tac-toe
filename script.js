3a03af3c8d45e73d04d3e67f31a5dbf8f00cbb02
Maxnikit <max.nikit.03@gmail.com>
2023-11-28 23:30:19 +0300

    Add table

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const table = document.querySelector("table");
+const rows = table.querySelectorAll("tr");
+const cells = table.querySelectorAll("td");
 const gameBoard = (function () {
   const rows = 3;
   const cols = 3;
@@ -6,7 +9,7 @@ const gameBoard = (function () {
     for (i = 0; i < rows; i++) {
       board[i] = [];
       for (j = 0; j < cols; j++) {
-        board[i][j] = 0;
+        board[i][j] = "";
       }
     }
     console.table(board);
@@ -16,32 +19,41 @@ const gameBoard = (function () {
 
   const getBoard = () => board;
   const putMark = (row, col, mark) => {
-    if (board[row][col] === 0) {
+    if (board[row][col] === "") {
       board[row][col] = mark;
     }
     console.table(board);
-  };
-  const printBoard = () => {
-    const boardWithMarks = board.map((row) =>
-      row.map((col) => (col === "" ? " " : col))
-    );
-    return console.table(boardWithMarks);
+    gameLogic.checkForWin(mark);
+    gameBoard.displayBoard();
   };
 
-  return { createBoard, getBoard, putMark, printBoard };
+  const displayBoard = () => {
+    array = gameBoard.getBoard().flat();
+    array.forEach((mark, i) => {
+      cells[i].textContent = mark;
+    });
+  };
+
+  return { createBoard, getBoard, putMark, displayBoard };
 })();
-const createPlayer = function (name) {
-  let score = 0;
-  const getScore = () => score;
-  const giveScore = () => {
-    score++;
+const playerLogic = (function () {
+  const createPlayer = function (name, mark) {
+    let score = 0;
+    const getScore = () => score;
+    const giveScore = () => {
+      score++;
+    };
+    return {
+      name,
+      mark,
+      getScore,
+      giveScore,
+    };
   };
-  return {
-    name,
-    getScore,
-    giveScore,
-  };
-};
+
+  return { createPlayer };
+})();
+
 const gameLogic = (function () {
   let board = gameBoard.getBoard();
 
@@ -88,10 +100,11 @@ const gameLogic = (function () {
     }
 
     if (threeInRow(board) || threeInCol(board) || threeInDiag(board)) {
-      if (mark === 1) winner = "X";
-      else if (mark === -1) winner = "O";
+      if (mark === "X") winner = "X";
+      else if (mark === "O") winner = "O";
       else return console.error("Winner is nor X, nor O");
       console.log(`Winner of the round is ${winner}`);
+      table.style.backgroundColor = "green";
       return true;
     }
     return false;
@@ -102,15 +115,33 @@ const gameLogic = (function () {
     gameBoard.createBoard();
   };
   // if (checkForWin(1 || -1)) startNewRound();
-
-  return { checkForWin, startNewRound };
+  const gameFlow = () => {
+    const player1 = playerLogic.createPlayer("Max", "X");
+    const player2 = playerLogic.createPlayer("Bob", "O");
+    // Осуществить выбор имени игрока и его маркировку
+    gameBoard.displayBoard();
+    let gameEnd = false;
+    while (!gameEnd) {
+      cells.forEach((cell) => {
+        cell.addEventListener("click", (e) => {
+          gameBoard.putMark(e.target.dataset.row, e.target.dataset.col, mark);
+        });
+      });
+    }
+    if (checkForWin("X") || checkForWin("O")) {
+      startNewRound();
+    }
+  };
+  return { checkForWin, startNewRound, gameFlow };
 })();
 
-const Max = createPlayer("Max");
-gameBoard.createBoard();
-gameBoard.putMark(1, 0, -1);
-gameBoard.putMark(1, 1, -1);
-gameBoard.putMark(1, 2, -1);
-// console.log(gameLogic.startNewRound());
-gameBoard.putMark(1, 1, 1);
+gameLogic.startNewRound();
+gameBoard.putMark(1, 0, "X");
+gameBoard.putMark(1, 1, "O");
+gameBoard.putMark(1, 2, "X");
+gameBoard.putMark(0, 1, "O");
+gameBoard.putMark(2, 1, "X");
 //
+
+gameBoard.displayBoard();
+gameLogic.gameFlow();
